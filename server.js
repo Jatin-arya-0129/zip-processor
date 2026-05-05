bebea67688506c37ff22bf68caa19c8c0a94f340
Jatin-arya-0129 <jatin9718858688@gmail.com>
2026-05-06 00:41:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const multer = require("multer");
 const fs = require("fs");
-const path = require("path");
 const AdmZip = require("adm-zip");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Storage
 const upload = multer({ dest: "uploads/" });
 
 let progress = 0;
 
-// Serve frontend
+// STATIC
 app.use(express.static("public"));
 
-// UPLOAD API
+// FIX FOR ROOT
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// UPLOAD
 app.post("/upload", upload.array("files"), (req, res) => {
   progress = 0;
 
@@ -38,7 +42,7 @@ app.post("/upload", upload.array("files"), (req, res) => {
   res.send("Done");
 });
 
-// PROGRESS API
+// PROGRESS
 app.get("/progress", (req, res) => {
   res.json({ progress });
 });
@@ -48,4 +52,4 @@ app.get("/download", (req, res) => {
   res.download("final.zip");
 });
 
-app.listen(PORT, () => console.log("Server running on port", PORT));
+app.listen(PORT, () => console.log("Server running"));
